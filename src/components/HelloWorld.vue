7b46bba3266e594584225605cb0a395444c646b6
Chao_Yan <>
2021-09-22 14:33:36 +0800

    upload

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,59 +1,150 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router" target="_blank" rel="noopener">router</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <v-app>
+    <v-app-bar color="primary" app>
+      <v-spacer></v-spacer>
+      <v-btn color="success">
+        {{ text.start_text }}
+      </v-btn>
+    </v-app-bar>
+    <v-main>
+      <v-container>
+        <v-row wrap fill-height>
+          <v-col md4 offset-md4 v-if="currentStep == 0">
+            <v-card>
+              <v-card-title>{{ text.following_step }}</v-card-title>
+              <v-card-text>
+                <v-list>
+                  <v-list-item v-for="item in testUnites" :key="item.id">
+                    <v-list-item-content>
+                      <v-list-item-title>{{ item.label }}</v-list-item-title>
+                    </v-list-item-content>
+                  </v-list-item>
+                </v-list>
+              </v-card-text>
+            </v-card>
+          </v-col>
+          <v-col md4 offset-md4 v-else>
+            <v-stepper>
+              <v-stepper-header>
+                <v-stepper-step :complete="e1 > 1" step="1">
+                  Name of step 1
+                </v-stepper-step>
+
+                <v-divider></v-divider>
+
+                <v-stepper-step :complete="e1 > 2" step="2">
+                  Name of step 2
+                </v-stepper-step>
+
+                <v-divider></v-divider>
+
+                <v-stepper-step step="3"> Name of step 3 </v-stepper-step>
+              </v-stepper-header>
+
+              <v-stepper-items>
+                <v-stepper-content step="1">
+                  <v-card
+                    class="mb-12"
+                    color="grey lighten-1"
+                    height="200px"
+                  ></v-card>
+
+                  <v-btn color="primary" @click="e1 = 2"> Continue </v-btn>
+
+                  <v-btn text> Cancel </v-btn>
+                </v-stepper-content>
+
+                <v-stepper-content step="2">
+                  <v-card
+                    class="mb-12"
+                    color="grey lighten-1"
+                    height="200px"
+                  ></v-card>
+
+                  <v-btn color="primary" @click="e1 = 3"> Continue </v-btn>
+
+                  <v-btn text> Cancel </v-btn>
+                </v-stepper-content>
+
+                <v-stepper-content step="3">
+                  <v-card
+                    class="mb-12"
+                    color="grey lighten-1"
+                    height="200px"
+                  ></v-card>
+
+                  <v-btn color="primary" @click="e1 = 1"> Continue </v-btn>
+
+                  <v-btn text> Cancel </v-btn>
+                </v-stepper-content>
+              </v-stepper-items>
+            </v-stepper>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
+import * as i18n from "../utils/i18n";
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
-</script>
+  name: "HelloWorld",
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+  data() {
+    return {
+      currentStep: 1,
+      testUnites: [
+        {
+          id: "0",
+          label: "browser_compatibility",
+          notError: true,
+          extra: "",
+        },
+        {
+          id: "1",
+          label: "microphone",
+          notError: true,
+          extra: "",
+        },
+        {
+          id: "2",
+          label: "speaker",
+          notError: true,
+          extra: "",
+        },
+        {
+          id: "3",
+          label: "resolution",
+          notError: true,
+          extra: "",
+        },
+        {
+          id: "4",
+          label: "connection",
+          notError: true,
+          extra: "",
+        },
+      ],
+    };
+  },
+  computed: {
+    text() {
+      const lang = "zh";
+      const property = i18n[lang]["default"];
+      const obj = {};
+      for (let key of Object.keys(property)) {
+        Object.assign(obj, {
+          [`${key}`]: property[key],
+        });
+      }
+      console.log("computed");
+      return obj;
+    },
+  },
+  mounted() {
+    console.log(this.text);
+    console.log("mounted");
+  },
+};
+</script>
