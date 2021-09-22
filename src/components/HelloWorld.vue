9b3a52110d2a16c61de24d50682ffda582b6984c
yy987y <375552740@qq.com>
2021-09-22 19:24:53 +0800

    improve

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,7 +2,7 @@
   <v-app>
     <v-app-bar color="primary" app>
       <v-spacer></v-spacer>
-      <v-btn color="success">
+      <v-btn color="success" @click="startTest">
         {{ text.start_text }}
       </v-btn>
     </v-app-bar>
@@ -24,15 +24,15 @@
             </v-card>
           </v-col>
           <v-col md4 offset-md4 v-else>
-            <v-stepper>
+            <v-stepper v-model="currentStep">
               <v-stepper-header>
-                <v-stepper-step :complete="e1 > 1" step="1">
+                <v-stepper-step :complete="currentStep > 0" step="1">
                   Name of step 1
                 </v-stepper-step>
 
                 <v-divider></v-divider>
 
-                <v-stepper-step :complete="e1 > 2" step="2">
+                <v-stepper-step :complete="currentStep > 1" step="2">
                   Name of step 2
                 </v-stepper-step>
 
@@ -42,40 +42,115 @@
               </v-stepper-header>
 
               <v-stepper-items>
+                <!-- mic test -->
                 <v-stepper-content step="1">
-                  <v-card
-                    class="mb-12"
-                    color="grey lighten-1"
-                    height="200px"
-                  ></v-card>
-
-                  <v-btn color="primary" @click="e1 = 2"> Continue </v-btn>
-
-                  <v-btn text> Cancel </v-btn>
+                  <v-row>
+                    <v-col md6>
+                      <v-card color="info" height="100%">
+                        <v-card-title>
+                          <div class="headline">
+                            {{ text.microphone_check }}
+                          </div>
+                        </v-card-title>
+                        <v-card-text>
+                          {{ text.microphone_check_desc }}
+                        </v-card-text>
+                      </v-card>
+                    </v-col>
+                    <v-col md6>
+                      <v-card color="info" height="100%">
+                        <v-card-title>
+                          {{ text.microphone_volume_check_desc }}
+                        </v-card-title>
+                        <v-card-text>
+                          <v-progress-linear
+                            :value="inputVolume"
+                          ></v-progress-linear>
+                        </v-card-text>
+                      </v-card>
+                    </v-col>
+                  </v-row>
                 </v-stepper-content>
 
+                <!-- speaker test -->
                 <v-stepper-content step="2">
-                  <v-card
-                    class="mb-12"
-                    color="grey lighten-1"
-                    height="200px"
-                  ></v-card>
+                  <v-row>
+                    <v-col md6>
+                      <v-card color="info" height="100%">
+                        <v-card-title>
+                          <div class="headline">
+                            {{ text.speacker_check }}
+                          </div>
+                        </v-card-title>
+                        <v-card-text>
+                          {{ text.speaker_check_desc }}
+                        </v-card-text>
+                        <v-btn @click="currentStep = 3"> Continue </v-btn>
 
-                  <v-btn color="primary" @click="e1 = 3"> Continue </v-btn>
-
-                  <v-btn text> Cancel </v-btn>
+                        <v-btn text color="error"> Cancel </v-btn>
+                      </v-card>
+                    </v-col>
+                    <v-col md6>
+                      <v-card color="info" height="100%">
+                        <v-card-title>
+                          <div class="headline">{{ text.sample_music }}</div>
+                        </v-card-title>
+                        <v-card-text>
+                          <audio id="sampleMusic" controls="controls">
+                            <source
+                              src="../assets/music.mp3"
+                              type="audio/mp3"
+                            />
+                            {{ text.sample_music_desc }}
+                          </audio>
+                        </v-card-text>
+                      </v-card>
+                    </v-col>
+                  </v-row>
                 </v-stepper-content>
 
+                <!-- resolution test -->
                 <v-stepper-content step="3">
-                  <v-card
-                    class="mb-12"
-                    color="grey lighten-1"
-                    height="200px"
-                  ></v-card>
-
-                  <v-btn color="primary" @click="e1 = 1"> Continue </v-btn>
-
-                  <v-btn text> Cancel </v-btn>
+                  <v-row>
+                    <v-col md6>
+                      <v-card color="info" height="100%">
+                        <v-card-title>
+                          <div class="headline">
+                            {{ text.resolution_check }}
+                          </div>
+                        </v-card-title>
+                        <v-card-text>
+                          {{ text.resolution_check_desc }}
+                          <v-btn @click="resolutionTest">test</v-btn>
+                        </v-card-text>
+                      </v-card>
+                    </v-col>
+                    <v-col md6>
+                      <v-card height="100%">
+                        <v-card-title>
+                           {{text.resolution_list}} </v-card-title>
+                        <v-card-text>
+                          <v-list>
+                            <v-list-item
+                              v-for="(profile,index) in profiles"
+                              :key="index"
+                            >
+                              <v-list-item-content>
+                                <v-list-item-title>{{
+                                  profile.resolution
+                                }}</v-list-item-title>
+                                <v-list-tile-action icon>
+                                  <v-icon v-if="profile.status==='resolve'" color="success">done</v-icon>
+                                  <v-icon v-else-if="profile.status==='reject'" color="error">close</v-icon>
+                                  <v-icon v-else>more_horiz</v-icon>
+                                </v-list-tile-action>
+                              </v-list-item-content>
+                            </v-list-item>
+                          </v-list>
+                        </v-card-text>
+                      </v-card>
+                    </v-col>
+                  </v-row>
                 </v-stepper-content>
               </v-stepper-items>
             </v-stepper>
@@ -88,12 +163,18 @@
 
 <script>
 import * as i18n from "../utils/i18n";
+import * as webRTC from "../utils/NIM_Web_NERTC_v4.4.1";
+import { profileArray } from "../utils/resolutionList";
 export default {
   name: "HelloWorld",
 
   data() {
     return {
-      currentStep: 1,
+      client: {},
+      localStream: {},
+      currentStep: 0,
+      inputVolume: 0,
+      uid: "",
       testUnites: [
         {
           id: "0",
@@ -126,6 +207,10 @@ export default {
           extra: "",
         },
       ],
+      profiles: profileArray.map((item) => {
+        item.status = "pending";
+        return item;
+      }),
     };
   },
   computed: {
@@ -138,13 +223,85 @@ export default {
           [`${key}`]: property[key],
         });
       }
-      console.log("computed");
       return obj;
     },
   },
   mounted() {
-    console.log(this.text);
-    console.log("mounted");
+    this.client = webRTC.createClient({
+      appkey: "4727023efa991d31d61b3b32e819bd5b",
+      debug: true,
+    });
+  },
+  methods: {
+    startTest() {
+      this.currentStep++;
+      this.audioTest();
+    },
+    audioTest() {
+      this.uid = Math.floor(Math.random() * 10000);
+      this.localStream = webRTC.createStream({
+        uid: this.uid,
+        audio: true,
+        video: true,
+        screen: false,
+      });
+
+      this.localStream.init().then(() => {
+        this.localStream
+          .play(null, {
+            audio: true,
+            video: false,
+            screen: false,
+          })
+          .then(() => {
+            console.warn("播放成功");
+          })
+          .catch((err) => {
+            console.warn("播放失败: ", err);
+          });
+        let _this = this;
+        let micTestTimer = setInterval(() => {
+          _this.inputVolume = parseInt(this.localStream.getAudioLevel() * 100);
+        }, 100);
+        setTimeout(() => {
+          clearInterval(micTestTimer);
+          console.log(this.localStream.getAudioTrack());
+          this.localStream.destroy();
+          // .then(() => {
+          //   console.warn("关闭 mic sucess");
+          // })
+          // .catch((err) => {
+          //   console.warn("关闭 mic 失败: ", err);
+          // });
+
+          this.currentStep = 2;
+        }, 3000);
+      });
+    },
+    speakerTest() {
+      console.log("speakerTest");
+    },
+    resolutionTest() {
+      let localStream = webRTC.createStream({
+        uid: Math.floor(Math.random() * 10000),
+        audio: false,
+        video: true
+      });
+      for (let profile of this.profiles) {
+        localStream.setVideoProfile({
+          profile: webRTC[`${profile.resolution}`],
+        });
+        localStream.init().then(() => {
+          console.log(`${profile.resolution} init success`);
+          profile.status="resolve";
+          localStream.destroy();
+        }).catch(()=>{
+          console.log(`${profile.resolution} init failed`);
+          profile.status="reject";
+          localStream.destroy();
+        });
+      }
+    },
   },
 };
 </script>
